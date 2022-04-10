12dc5150e62c26c2c7b828d56f401f5e7604598a
Geoffrey Jarman <geoff@Geoffreys-MacBook-Pro.local>
2022-04-10 10:16:42 -0500

    adding gord battles

--- src/battle/battle.controller.js
+++ src/battle/battle.controller.js
@@ -1,90 +1,78 @@
-let currentPlayers = 0;
-let current = 0
-const games = [];
+const words = require("../../words.js");
+let games = [];
 
-const get = (req, res) => {
+const sendGameData = (req, res, next) => {
     const { token } = req.query;
-    const currentGame = games.find(g => g.token == token);
-    if (currentGame) {
-        if (currentGame.players > 1) {
-            clearTimeout(currentGame.timeout);
-            currentGame.current = 0
-            currentGame.counter = setInterval(() => {
-                currentGame.current++
-                console.log(currentGame.current)
-                if (currentGame.current > 30) {
-                    clearInterval(currentGame.counter)
-                    const index = games.indexOf(currentGame);
-                    if (index > -1) {
-                        games.splice(index, 1);
-                    }
-                    console.log(games)
-                }
-            },1000)
-            return res.status(200).json({data: "timer started"})
+    if (token) {
+        const game = games.find(g => g.token == token);
+        games = games.filter(g => g.timeout == false);
+        return res.status(202).json({...game}) 
+    }  else if (games.some(g => g.players.length < 2 && g.timeout == false)){
+        for (let game of games) {
+            if (game.players.length < 2) {
+                game.players.push({
+                    playerName: 2,
+                    lastGuess: ''
+                })
+                return res.status(202).json({...game}) 
+            }
         }
-        return res.status(300).json({data: "Player 2 not connected"})
-    }
-    return res.status(300).json({data: "No game active"})
-}
-const keepAlive = (currentGame) => {
-    if (currentGame) {
-        console.log(currentGame.timeout)
-        clearTimeout(currentGame.timeout);
-    }
-}
-const addPlayer = (req, res) => {
-    const { token } = req.query;
-    const currentGame = games.find(g => g.token == token);
-    if (currentGame) {
-        keepAlive(currentGame);
-        currentGame.players++
-        return res.status(200).json({data: {
-            message: "Player Added",
-            game: currentGame
-        }})
     } else {
         const newGame = {
-            players: 1,
             token: Date.now(),
-            index: games.length,
-            setKillTimer: (token) => {
-                return setTimeout(() => {
-                    const index = games.indexOf(games.find(game => game.token === token));
-                    if (index > -1) {
-                        games.splice(index, 1);
-                    }
-                    console.log(games, this)
-                }, 10000)
-            },
+            players: [
+                {
+                    playerName: 1,
+                    lastGuess: ''
+                }
+            ],
+            winner: null,
+            word: words[Math.floor(Math.random() * words.length)],
+            timeout: false,
+            isRunning: false
         }
-        newGame['timeout'] = newGame.setKillTimer(newGame.token);
-        games.push(newGame);
-        return res.status(200).json({data: {
-            message: "Lobby Created",
-            game: newGame.token
-        }})
+        games.push(newGame)
+        setTimeout(() => {
+            if (!newGame.isRunning) {
+                newGame.timeout = true;
+            }
+        }, 60000)
+        return res.status(202).json(newGame)
     }
 }
+const startGame = (req, res, next) => {
+    const { token, isRunning } = req.body;
+    const game = games.find(g => g.token == token);
+    game.isRunning = true;
+    game.killTime = game.killTime ? game.killTime : Date.now() + 60000;
 
-const updateGame = (req, res) => {
-    const { token, message } = req.body;
-    const currentGame = games.find(g => g.token == token);
-    if (currentGame.messages) {
-        currentGame.messages.push(message)
-    } else {
-        currentGame.messages = [message]
-    }
-    keepAlive(currentGame)
-    console.log(req.body);
-    return res.status(202).json({data: currentGame.messages})
+    setTimeout(() => {
+        game.isRunning = false;
+    }, 60000)
+    return res.status(200).json({...game})
+
 }
-const getCurrent = (req, res) => {
-    return res.status(200).json({data: current})
+const updateGame = (req, res) => {
+    const data = req.body;
+    const { player } = req.query
+    const game = games.find(g => g.token == data.token);
+    game.players[player] = data.players[player];
+    game.players.forEach(player => {
+        if (player.lastGuess === game.word) {
+            game.winner = player;
+            game.isRunning = false;
+            game.timeout = true;
+        }
+    })
+    return res.status(200).json({...game})
+}
+const clearGames = (req, res) => {
+    games = [];
+    return res.sendStatus(204);
 }
 module.exports = {
-    get,
-    getCurrent,
-    addPlayer,
-    updateGame
+    sendGameData,
+    startGame,
+    updateGame,
+    clearGames
 }
--- src/battle/battle.router.js
+++ src/battle/battle.router.js
@@ -2,8 +2,11 @@ const router = require("express").Router();
 const controller = require("./battle.controller.js");
 const methodNotAllowed = require("../errors/methodNotAllowed.js");
 
-router.route("/").get(controller.addPlayer).post(controller.updateGame).all(methodNotAllowed);
-router.route("/start").get(controller.getCurrent).all(methodNotAllowed);
-router.route("/addPlayer").get(controller.addPlayer).all(methodNotAllowed);
+router.route("/")
+    .get(controller.sendGameData)
+    .post(controller.startGame)
+    .put(controller.updateGame)
+    .delete(controller.clearGames)
+    .all(methodNotAllowed);
 
 module.exports = router;
